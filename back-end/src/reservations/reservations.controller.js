079ee17e13d25e73cae887a0b177db8f19acf401
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-26 17:41:16 -0400

    update controller, router add service file

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,23 @@
-/**
- * List handler for reservation resources
- */
-async function list(req, res) {
-  res.json({
-    data: [],
-  });
+const service = require("./reservations.service");
+
+async function create(req, res, next) {
+  const reservation = req.body.data;
+  try {
+    const newReservation = await service.create(reservation);
+    res.status(201).json({ data: newReservation });
+  } catch (error) {
+    next(error);
+  }
 }
 
+async function list(req, res) {
+  const { date } = req.query;
+  const reservations = await service.list(date);
+  res.json({ data: reservations });
+}
+
+
 module.exports = {
+  create,
   list,
 };
---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,21 @@
+const knex = require("../db/connection");
+
+function create(reservation) {
+  return knex("reservations")
+    .insert(reservation)
+    .returning("*")
+    .then((reservations) => reservations[0]);
+}
+
+function list(date) {
+    return knex("reservations")
+      .select("*")
+      .where({ reservation_date: date })
+      .orderBy("reservation_time");
+  }
+  
+
+module.exports = {
+  create,
+  list,
+};
